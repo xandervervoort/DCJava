ecf8db832a823fb8c7f455cb22c08b097675f279
xander <51018130+xandervervoort@users.noreply.github.com>
2023-08-07 11:43:54 +0200

    Update delete_command.js

--- src/delete_command.js
+++ src/delete_command.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const applicationId = '1093189880600866967';
-const token = 'MTA5MzE4OTg4MDYwMDg2Njk2Nw.GVpFu7.-gqshoP8brSx9M8hi4C4G59oANRdFgNibtlG94';
+const token = '';
 const commandIdToRemove = '1134810547838271499';
 
 const url = `https://discord.com/api/v9/applications/${applicationId}/commands/${commandIdToRemove}`;
